cd98cf1cecab639d1e95bc942fda76df5582e12f
mohamedelkhiat <mohamednabilelkhiate@gmail.com>
2023-09-10 17:32:11 -0700

    h

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main -Entery point
+ * main - Entery point
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        printif("with proper grammar, but the outcome is a piece of art,"\n)
-	return (0);
-}	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
