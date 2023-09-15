504a55d9470413a403540fa1ad40190d98f65461
mohamedelkhiat <mohamednabilelkhiate@gmail.com>
2023-09-14 18:02:44 -0700

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * _isupper - check if c isupper
+ *
+ * @c: input of alphapet
+ *
+ * return: 1 if its uppercase, 0 if not
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
