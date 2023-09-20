d9ff4bc82a9a6cef96ed99ef96e24cc6697257a0
mohamedelkhiat <mohamednabilelkhiate@gmail.com>
2023-09-19 17:46:57 -0700

    kk

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - Returns the lenght of a string.
- * @str: The string to get the lenght of 
+ * @str: The string to get the lenght of
  * Return: The lenght of @str.
  */
 size_t _strlen(const char *str)
