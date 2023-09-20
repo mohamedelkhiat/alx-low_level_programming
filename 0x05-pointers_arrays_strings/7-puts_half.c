19df2d0cddea7f0a7390e7ae6614844cae34ecae
mohamedelkhiat <mohamednabilelkhiate@gmail.com>
2023-09-19 19:02:17 -0700

    lk

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,14 +18,13 @@ void puts_half(char *str)
 	}
 	if (j % 2 == 1)
 	{
-	k = (j - 1) / 2; 
+	k = (j - 1) / 2;
 	k += 1;
 	}
 	else
 	{
 		k = j / 2;
 	}
-	
 	for (; k < j; k++)
 	{
 	_putchar(str[k]);
